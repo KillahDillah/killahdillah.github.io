d833b3beb8e7be00d92908b23363533c706ba918
Leah Miller <leahmiller1969@gmail.com>
2017-12-22 22:41:26 -0800

    new bio

--- main.js
+++ main.js
@@ -6,10 +6,29 @@ function carousel() {
     var i;
     var x = document.getElementsByClassName("slides");
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+       // x[i].style.opacity = 0;
+       x[i].style.display = "none";
+       // x[i].style.animationTimingFunction = "ease-in";
+       // x[i].style.animationDuration = "5s"
+       // x[i].style.transition = "opacity .5s ease-out"  
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
+    // x[myIndex-1].style.opacity = 1;
+    x[myIndex-1].style.display = "block";
+    // x[myIndex-1].style.animationTimingFunction = "easeout";
+    // x[myIndex-1].style.animationDuration = "3s"
+    // x[myIndex-1].style.transition= "opacity .5s ease-in";  
     setTimeout(carousel, 3500); // Change image every 2 seconds
 }
+
+// var current = 0,
+//     slides = document.getElementsByClassName("slides");
+
+// setInterval(function() {
+//   for (var i = 0; i < slides.length; i++) {
+//     slides[i].style.opacity = 0;
+//   }
+//   current = (current != slides.length - 1) ? current + 1 : 0;
+//   slides[current].style.opacity = 1;
+// }, 3000);
